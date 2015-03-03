4adee2ae5f462b3db81be489b888e957405009ba
Florin <florin.coros@iquarc.com>
2015-03-03 11:39:21 +0200

    switch to SemVer versioning

--- AppBoot/iQuarc.AppBoot/Properties/AssemblyInfo.cs
+++ AppBoot/iQuarc.AppBoot/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("iQuarc.AppBoot")]
 [assembly: AssemblyDescription("Basic functionality for hiding the DI Container and for defining a modular application.")]
 [assembly: AssemblyConfiguration("")]
@@ -19,31 +18,64 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("5df84816-eaa2-460c-8068-96a2b1a066ee")]
 
 // This assembly code is intended to be used from other languages than C#
-
 [assembly: CLSCompliant(true)]
 
 // Make the Unit Testing assembly friendly
-
 [assembly: InternalsVisibleTo("iQuarc.AppBoot.UnitTests")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+// Versioning:
+//		Adopting SemVer both for NuGet packages versions and also for Assembly Version  (http://semver.org/ | https://docs.nuget.org/create/versioning). 
+//		We prefer nuspec to take the package version from assembly version.
+[assembly: AssemblyVersion(Version.Assembly)]
+[assembly: AssemblyFileVersion(Version.Assembly)]
+[assembly: AssemblyInformationalVersion(Version.NugetReleasePackage)]
+
+static class Version
+{
+	/// <summary>
+	///     Breaking changes.
+	/// </summary>
+	private const string Major = "1";
+
+	/// <summary>
+	///     New features, but backwards compatible.
+	/// </summary>
+	private const string Minor = "1";
+
+	/// <summary>
+	///     Backwards compatible bug fixes only.
+	/// </summary>
+	private const string Patch = "0";
+
+	/// <summary>
+	///     Build number. Prefix with 0 for NuGet version ranges
+	/// </summary>
+	private const string Build = "000";
+
+	/// <summary>
+	///     NuGet Pre-Release package versions
+	/// </summary>
+	private const string Prerelease = "test";
+
+	/// <summary>
+	/// Used to set the assembly version
+	/// </summary>
+	public const string Assembly = Major + "." + Minor + "." + Patch + "." + Build;
+
+	/// <summary>
+	/// Used to set the version of a release NuGet Package
+	/// </summary>
+	public const string NugetReleasePackage = Major + "." + Minor + "." + Patch;
+
+	/// <summary>
+	/// Used to set the version of a pre-release NuGet Package
+	/// </summary>
+	public const string NugetPrereleasePackage = Major + "." + Minor + "." + Patch + "-" + Prerelease+Build;
+}
