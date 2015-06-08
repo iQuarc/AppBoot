18afbb9dd100ae2f26e01f9916cd78307db71a69
Catalin Pop <pop.catalin@gmail.com>
2015-06-08 22:15:33 +0300

    Added Wcf Hosting Registration Behavior

---
+++ AppBoot/iQuarc.AppBoot.WcfHosting/WcfHostingRegistrationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using iQuarc.SystemEx;
+using System.ServiceModel.Activation;
+using System.Web.Routing;
+
+namespace iQuarc.AppBoot.WcfHosting
+{
+    public class WcfHostingRegistrationBehavior : IRegistrationBehavior
+    {
+        string baseAddress;
+        public WcfHostingRegistrationBehavior(string baseAddress)
+        {
+            this.baseAddress = baseAddress;
+        }
+
+        public IEnumerable<ServiceInfo> GetServicesFrom(Type type)
+        {
+            IEnumerable<WcfServiceAttribute> attributes = type.GetAttributes<WcfServiceAttribute>(false);
+            var services = attributes.Select(a =>
+                new ServiceInfo(a.ContractType, type, Lifetime.AlwaysNew));
+            foreach (var service in services)
+            {
+                RouteTable.Routes.Add(new ServiceRoute(baseAddress, new WebServiceHostFactory(), type));
+            }
+            return services;
+        }
+    }
+}
